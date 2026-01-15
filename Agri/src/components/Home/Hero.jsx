689d0353374a42458ccc44426050432e0e7df90f
janhavik06 <janhavikundgir06@gmail.com>
2026-01-15 16:27:53 +0530

    testing

--- Agri/src/components/Home/Hero.jsx
+++ Agri/src/components/Home/Hero.jsx
@@ -1,12 +1,12 @@
-import React from 'react'
+import React from "react";
 
 function Hero() {
   return (
     <div>
       Hero
-     <h1>Home page</h1>
+      <h1>Home page test </h1>
     </div>
-  )
+  );
 }
 
-export default Hero
+export default Hero;
