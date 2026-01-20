90fc04981c4fd4a6bbb6b89535a2786e209f0789
janhavik06 <janhavikundgir06@gmail.com>
2026-01-20 11:33:51 +0530

    done with app page

--- Agri/src/App.jsx
+++ Agri/src/App.jsx
@@ -1,15 +1,12 @@
-import React from 'react'
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
-import HomePage from './pages/HomePage.jsx'
-import AboutPage from './pages/AboutPage.jsx'
-import ServicesPage from './pages/ServicesPage.jsx'
-import ProjectPage from './pages/Projectpage.jsx'
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import HomePage from "./pages/HomePage.jsx";
+import AboutPage from "./pages/AboutPage.jsx";
+import ServicesPage from "./pages/ServicesPage.jsx";
+import ProjectPage from "./pages/Projectpage.jsx";
 
-
-
-import Navbar from './components/navbar/Navbar.jsx'
-import Footer from './components/Footer/Footer.jsx'
-
+import Navbar from "./components/navbar/Navbar.jsx";
+import Footer from "./components/Footer/Footer.jsx";
 
 function App() {
   return (
@@ -20,14 +17,14 @@ function App() {
           <Routes>
             <Route path="/" element={<HomePage />} />
             <Route path="/about" element={<AboutPage />} />
-            <Route path="/services" element={<ServicesPage />} />
+            <Route path="/services/*" element={<ServicesPage />} />
             <Route path="/Projects" element={<ProjectPage />} />
           </Routes>
         </main>
         <Footer />
       </div>
     </Router>
-  )
+  );
 }
 
-export default App
+export default App;
